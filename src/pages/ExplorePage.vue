654a992731dae4a595a1a8de4771301f49d8a0c5
10zinten <ten13zin@gmail.com>
2022-10-08 16:58:02 +0530

    add explore page

---
+++ src/pages/ExplorePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-page padding>
+    <div>
+      <q-tabs v-model="tab" inline-label class="text-primary">
+        <q-tab name="recommended" icon="recommend" label="Recommended" />
+        <q-tab name="search" icon="search" label="Search" />
+      </q-tabs>
+    </div>
+
+    <div>
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="recommended">
+          <div class="q-gutter-md">
+            <q-card v-for="course in recommendedCourses" :key="course.id">
+              <q-video :src="course.src" />
+              <q-card-section>
+                <div class="text-h6">{{ course.title }}</div>
+              </q-card-section>
+
+              <q-separator />
+
+              <q-card-actions align="around">
+                <q-btn flat icon="headphones" label="Start" />
+                <q-btn flat icon="bookmark" label="Save" />
+              </q-card-actions>
+            </q-card>
+          </div>
+        </q-tab-panel>
+        <q-tab-panel name="search">
+          <q-input v-model="search" placeholder="Search here" />
+          {{ search }}
+        </q-tab-panel>
+      </q-tab-panels>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const tab = ref('recommneded');
+const search = ref('');
+
+const genrateDummyCourse = (n: number) => {
+  const courses = [];
+  const course = {
+    id: n,
+    title: `Meditation & Mindfulness | Day ${n}`,
+    src: 'https://www.youtube.com/embed/EzntoqdlK7Q',
+  };
+  for (let i = 0; i < n; i++) {
+    courses.push(course);
+  }
+  return courses;
+};
+
+const recommendedCourses = genrateDummyCourse(10);
+</script>
